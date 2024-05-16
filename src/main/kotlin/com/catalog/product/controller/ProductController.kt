72ee7766a683a6a53f714d2a2d06d64322dbb672
rdilruba <dilrubakose@gmail.com>
2024-05-17 00:17:44 +0200

    remove unused import

--- src/main/kotlin/com/catalog/product/controller/ProductController.kt
+++ src/main/kotlin/com/catalog/product/controller/ProductController.kt
@@ -4,7 +4,6 @@ import com.catalog.product.model.Product
 import com.catalog.product.service.ProductService
 import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
